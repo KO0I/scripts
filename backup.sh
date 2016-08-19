74084888b17f851a114eae25ab65e2c153123eeb
KD0SKH <patrick91jh@gmail.com>
2016-08-18 19:22:31 -0600

    better backup script

--- backup.sh
+++ backup.sh
@@ -1,30 +1,42 @@
 # Please run this script as an administrative user
 # Check if backup disk is mounted
 backupdir=$(/media/Linux\ Backups/backups)
-if [ -d /media/Linux\ Backups/backups ]; then
-#if [ -d /media/Linux Backups/backups ]; then
-  filename=$(LC_ALL=C date +"%m_%d_%y")
+#if [ -d /media/Linux\ Backups/backups ]; then
+if [ -d /media/Desktop\ Backup/patrick/backups ]; then
+  filename=$HOST$(LC_ALL=C date +"%m_%d_%y")
   echo "Creating backup directory" $filename
-  mkdir /media/Linux\ Backups/backups/$filename
-  if [ -d /media/Linux\ Backups/backups/$filename ]; then
-    echo "done!"
-    # begin backing up every important directory
-    echo "backing up /boot ------------------------------------------------"
-    tar -zcf /media/Linux\ Backups/backups/$filename/boot.tgz /boot
-    echo "backing up /etc ------------------------------------------------"
-    tar -zcf /media/Linux\ Backups/backups/$filename/etc.tgz /etc
-    echo "backing up /opt ------------------------------------------------"
-    tar -zcf /media/Linux\ Backups/backups/$filename/opt.tgz /opt
-    echo "backing up /root ------------------------------------------------"
-    tar -zcf /media/Linux\ Backups/backups/$filename/root.tgz /root
-    echo "backing up /home ------------------------------------------------"
-    tar -zcf /media/Linux\ Backups/backups/$filename/home.tgz /home
+  #mkdir /media/Linux\ Backups/backups/$filename
+  mkdir /media/Desktop\ Backup/patrick/backups/$filename
+  #if [ -d /media/Linux\ Backups/backups/$filename ]; then
+  #  echo "done!"
+  #  # begin backing up every important directory
+  #  echo "backing up /boot ------------------------------------------------"
+  #  tar -zcf /media/Linux\ Backups/backups/$filename/boot.tgz /boot
+  #  echo "backing up /etc ------------------------------------------------"
+  #  tar -zcf /media/Linux\ Backups/backups/$filename/etc.tgz /etc
+  #  #echo "backing up /opt ------------------------------------------------"
+  #  #tar -zcf /media/Linux\ Backups/backups/$filename/opt.tgz /opt
+  #  echo "backing up / (as
+  #  root.tgz) ------------------------------------------------"
+  #  tar -zcf /media/Linux\ Backups/backups/$filename/root.tgz /
+  #  --exclude='proc*'
+  #  echo "backing up /home ------------------------------------------------"
+  #  tar -zcf /media/Linux\ Backups/backups/$filename/home.tgz /home
+  #  echo "backing up a list of all installed packages to pkglist.txt"
+  #  pacman -Qqen > /media/Linux\ Backups/backups/$filename/pkglist.txt
+
+  # USING RSYNC INSTEAD
+#  rsync -aAXv --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found"} / /media/Linux\ Backups/backups/$filename 
+  
+  if [ -d /media/Desktop\ Backup/patrick/backups/$filename ]; then
+    echo "Creating backup directory" $filename " on Desktop Backup"
+    rsync -aAXv --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found"} / /media/Desktop\ Backup/patrick/backups/$filename 
     echo "backing up a list of all installed packages to pkglist.txt"
-    pacman -Qqen > /media/Linux\ Backups/backups/$filename/pkglist.txt
-    echo "all done!"
+    pacman -Qqen > /media/Desktop\ Backup/patrick/backups/$filename/pkglist.txt
   else
     echo "Could not create the directory"
   fi
+
 else
   echo "Backup disk not found!"
 fi
