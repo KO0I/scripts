995d8ad6aaa761c71b2f77d6890e60cefd10cdc6
ko0i <ko0i@protonmail.com>
2018-02-19 22:32:22 -0700

    major enhancement of colorshuffle; now produces a color distinct from previous terminal

--- reflect.sh
+++ reflect.sh
@@ -2,5 +2,5 @@ echo "Creating backup of current mirrorlist as mirrorlist.bak2"
 
 cp -vf /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
 
-#reflector --verbose --country 'United States' -l 200 -p http --sort rate --save /etc/pacman.d/mirrorlist
-reflector --verbose --country 'Germany' -l 200 -p http --sort rate --save /etc/pacman.d/mirrorlist
+reflector --verbose --country 'United States' -l 200 -p http --sort rate --save /etc/pacman.d/mirrorlist
+#reflector --verbose --country 'Germany' -l 200 -p http --sort rate --save /etc/pacman.d/mirrorlist
